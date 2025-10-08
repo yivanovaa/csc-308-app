c26fd02e675658b2f664b73d26979dfb6c2991b4
Yanitsa Ivanova <yivanova@calpoly.edu>
2025-10-07 22:27:11 -0700

    added post call to add users

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -12,11 +12,27 @@ function MyApp() {
     };
 
     function updateList(person) {
-        setCharacters([...characters, person]);
+        postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+            console.log(error);
+        })
     };
 
     function fetchUsers() {
         const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
+    function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+        });
+
         return promise;
     }
 
